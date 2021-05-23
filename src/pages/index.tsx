2f38bafb257892fe129198f74003c6705e54abbc
Okoronkwo Chibueze <ricobuez@gmail.com>
2021-05-23 08:49:18 +0100

    init git

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>I am Simdi</title>
+        <meta name="description" content="I am Simdi and I am a Model" />
+        <link rel="icon" href="/images/logo.ico" />
+      </Head>
+
+      <header></header>
+      <main></main>
+    </div>
+  );
+}
