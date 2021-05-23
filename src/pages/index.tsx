b443e50130509944228317b8201296e9556a02cf
Okoronkwo Chibueze <ricobuez@gmail.com>
2021-05-23 13:25:45 +0100

    almost done with the header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,33 @@ export default function Home() {
         </nav>
 
         <div className="content">
-          <div className="container inner"></div>
+          <div className="social-icons">
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-twitter"></i>
+          </div>
+          <div className="container inner">
+            <div className="name mb-4">
+              <p className="joy">JOY</p>
+              <p className="simdi">SIMDI</p>
+            </div>
+
+            <div className="title text-center text-light">
+              <h4>Nigerian Supermodel &amp; Enterpreneur</h4>
+            </div>
+            <div className="arrow-down">
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path d="M11 21.883l-6.235-7.527-.765.644 7.521 9 7.479-9-.764-.645-6.236 7.529v-21.884h-1v21.883z" />
+              </svg>
+            </div>
+          </div>
         </div>
       </header>
 
