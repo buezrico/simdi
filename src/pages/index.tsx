f710595458d7482806f796886b93472eec6d53c3
Okoronkwo Chibueze <ricobuez@gmail.com>
2021-05-24 17:11:49 +0100

    created the about page seperately

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import AboutPage from "./about";
 
 export default function Home() {
   return (
@@ -54,30 +55,6 @@ export default function Home() {
           </div>
         </div>
       </header>
-
-      <main className="about">
-        <div className="inner">
-          <div className="container">
-            <div className="about-text">
-              <div className="about-title">
-                <h4 className="mb-4 styled-text">About Me</h4>
-              </div>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
-                eaque autem similique eveniet magni? Amet alias illo ipsum illum
-                rerum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Praesentium, magni placeat atque sed non quasi, unde quod
-                reiciendis, deleniti architecto vel mollitia libero ex. Porro
-                earum inventore et asperiores unde fugiat, doloremque quisquam
-                laudantium voluptas, voluptate nisi tenetur ea! Suscipit!
-              </p>
-            </div>
-            {/* <div className="about-pic"></div> */}
-          </div>
-        </div>
-      </main>
-
-      <section className="section"></section>
     </div>
   );
 }
