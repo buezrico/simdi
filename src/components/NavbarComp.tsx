d00b87ee7243d3b743225a0fda9aecbe52f9d1c9
Okoronkwo Chibueze <ricobuez@gmail.com>
2021-05-26 16:05:03 +0100

    working on the nav

--- src/components/NavbarComp.tsx
+++ src/components/NavbarComp.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { gsap } from "gsap";
 
 const NavbarComp = () => {
   const [nav, setNav] = useState(false);
+
+  const tween = gsap.from(".menu", {
+    duration: 1.5,
+    ease: "elastic.out(1, 0.3)",
+    top: -90,
+    opacity: 0,
+    delay: 1,
+  });
+
   return (
     <nav className="navbar text-primary">
       <div className="container">
@@ -12,14 +22,14 @@ const NavbarComp = () => {
         </div>
 
         <button className="btn rounded-circle btn-outline-primary py-1">
-          <i className="fas fa-bars" onClick={() => setNav(!nav)}></i>
+          <i className="fas fa-bars"></i>
         </button>
 
         <button className="btn rounded-0 btn-outline-primary px-md-4">
           Book Me
         </button>
 
-        <div className="menu" style={{ top: nav ? 50 : "" }}>
+        <div className="menu" style={{}}>
           <div className="container menu-wrapper">
             <div className="menu-items">
               <div className="navitem home-link">
@@ -35,7 +45,7 @@ const NavbarComp = () => {
                 <a className="nav-link">Contact Me</a>
               </div>
               <div className="close navitem d-flex">
-                <button className="btn rounded-circle btn-primary text-light p-2 py-1">
+                <button className="btn rounded-circle btn-primary text-light px-3 py-1">
                   <i className="fas fa-times" onClick={() => setNav(!nav)}></i>
                 </button>
               </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,77 @@
 import Head from "next/head";
-import React from "react";
+import React, { Component } from "react";
+import { TimelineLite, Bounce } from "gsap";
 import NavbarComp from "../components/NavbarComp";
 import AboutPage from "../components/AboutComp";
 import GalleryComp from "../components/GalleryComp";
 import FooterComp from "../components/FooterComp";
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>I am Simdi</title>
-        <meta name="description" content="I am Simdi and I am a Model" />
-        <link rel="icon" href="/images/logo.ico" />
-      </Head>
+class Simdi extends Component {
+  componentDidMount() {
+    const tl = new TimelineLite();
+    tl.from(
+      ".joy",
+      {
+        duration: 1.5,
+        ease: Bounce.easeOut,
+        y: -100,
+        opacity: 0,
+        delay: 1,
+      },
+      ""
+    );
+    tl.from(".simdi", {
+      duration: 1.5,
+      ease: Bounce.easeOut,
+      y: 100,
+      opacity: 0,
+    });
+  }
 
-      <header>
-        <NavbarComp />
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>I am Simdi</title>
+          <meta name="description" content="I am Simdi and I am a Model" />
+          <link rel="icon" href="/images/logo.ico" />
+        </Head>
 
-        <div className="content">
-          <div className="container inner">
-            <div className="name mb-4">
-              <p className="joy styled-text">JOY</p>
-              <p className="simdi styled-text">SIMDI</p>
-            </div>
+        <header>
+          <NavbarComp />
 
-            <div className="title text-center text-light mb-3">
-              <h4>Nigerian Supermodel &amp; Enterpreneur</h4>
-            </div>
-            <div className="arrow-down">
-              <svg
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="white"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-              >
-                <path d="M11 21.883l-6.235-7.527-.765.644 7.521 9 7.479-9-.764-.645-6.236 7.529v-21.884h-1v21.883z" />
-              </svg>
+          <div className="content">
+            <div className="container inner">
+              <div className="name mb-4">
+                <p className="joy styled-text">JOY</p>
+                <p className="simdi styled-text">SIMDI</p>
+              </div>
+
+              <div className="title text-center text-light mb-3">
+                <h4>Nigerian Supermodel &amp; Enterpreneur</h4>
+              </div>
+              <div className="arrow-down">
+                <svg
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="white"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                >
+                  <path d="M11 21.883l-6.235-7.527-.765.644 7.521 9 7.479-9-.764-.645-6.236 7.529v-21.884h-1v21.883z" />
+                </svg>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <GalleryComp />
-      <AboutPage />
-      <FooterComp />
-    </div>
-  );
+        <GalleryComp />
+        <AboutPage />
+        <FooterComp />
+      </div>
+    );
+  }
 }
+
+export default Simdi;
