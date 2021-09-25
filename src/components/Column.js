440a13e34ff71c83268301c9f39751731ce8e858
buezrico <ricobuez@gmail.com>
2021-09-25 10:30:16 +0100

    started working on the gallery section

---
+++ src/components/Column.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImgFrame from "./ImgFrame";
+
+function Column() {
+  return (
+    <div>
+      <ImgFrame />
+      First Column
+    </div>
+  );
+}
+
+export default Column;
---
+++ src/components/ImgFrame.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ImgFrame() {
+  return (
+    <div>
+      <h1>Img Frame</h1>
+    </div>
+  );
+}
+
+export default ImgFrame;
